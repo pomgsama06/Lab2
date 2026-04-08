40cd52d24740bdd6bee5e27c7b1f8e30d227f702
Misaka_Mikoto <pongsama@snu.ac.kr>
2026-04-08 13:40:59 +0900

    s

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int cnt, int start) {
+    if (cnt == M) {
+        for (int x : vec) cout << x << ' ';
+        cout << '\n';
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1, i + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0, 1);
 }
